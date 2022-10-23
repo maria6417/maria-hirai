9302a63af56c6393c74e79da1d4d36eaf828bb30
hmaria <mh6417a@gmail.com>
2022-10-23 10:34:37 -0700

    fixed css for Nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,59 +4,93 @@ import logo from '../assets/m.png';
 
 export default function Header() {
   return (
-    <FlexHeader justifyContent="space-between" alignItems="center" width="90%" margin="auto" style={{ padding: '20px' }}>
-      <Logo src={logo} />
-      <Nav justifyContent="space-between" alignItems="center" style={{ 'max-width': '40%' }}>
-        <LinkItem href=""><Span>01. </Span>About</LinkItem>
-        <LinkItem href=""><Span>02. </Span>Experience</LinkItem>
-        <LinkItem href=""><Span>03. </Span>Works</LinkItem>
-        <Button type="button">Resume</Button>
+    <HeaderContainer>
+      <Nav>
+        <Logo src={logo} />
+        <Flex>
+          <Links>
+            <LinkItem href="">
+              <Span>01. </Span>
+              About
+            </LinkItem>
+            <LinkItem href="">
+              <Span>02. </Span>
+              Experience
+            </LinkItem>
+            <LinkItem href="">
+              <Span>03. </Span>
+              Works
+            </LinkItem>
+          </Links>
+          <Button type="button">Resume</Button>
+        </Flex>
       </Nav>
-    </FlexHeader>
-  )
+    </HeaderContainer>
+  );
 }
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  position: fixed;
+  top: 0px;
+  z-index: 11;
+  height: 100px;
+  width: 100%;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  position: relative;
+`;
 
 const Logo = styled.img`
   height: 50px;
   width: 50px;
 `;
 
-const Span = styled.span`
-  color: ${props => props.theme.main}
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: ${(props) => props.theme.xs};
 `;
 
-const LinkItem = styled.a`
+const LinkItem = styled.li`
   color: black;
+  padding: 10px;
+  margin: 0 5px;
   &:hover {
-    color: ${props => props.theme.main};
+    color: ${(props) => props.theme.main};
+    opacity: 0.5;
+    cursor: pointer;
   }
 `;
 
-const FlexHeader = styled.header`
+const Links = styled.ol`
   display: flex;
-  justify-content: ${props => props.justifyContent ? props.justifyContent : 'initial'};
-  align-items: ${props => props.alignItems ? props.alignItems : 'initial'};
-  flex-direction: ${props => props.dir ? props.dir : 'row'};
-  width: ${props => props.width ? props.width : '100%'};
-  margin: ${props => props.margin ? props.margin : 0};
-  position: fixed;
-  top: 0px;
-  z-index: 11;
+  justify-content: space-between;
+  align-items: center;
+  /* max-width: 40%; */
 `;
 
-const Nav = styled.nav`
-  display: flex;
-  justify-content: ${props => props.justifyContent ? props.justifyContent : 'initial'};
-  align-items: ${props => props.alignItems ? props.alignItems : 'initial'};
-  flex-direction: ${props => props.dir ? props.dir : 'row'};
-  width: ${props => props.width ? props.width : '100%'};
-  margin: ${props => props.margin ? props.margin : 0};
+const Span = styled.span`
+  color: ${(props) => props.theme.main};
 `;
 
 const Button = styled.button`
   background-color: white;
-  color: ${props => props.theme.main};
-  border: ${props => `1px solid ${props.theme.main}`};
+  color: ${(props) => props.theme.main};
+  border: ${(props) => `1px solid ${props.theme.main}`};
   border-radius: 5px;
-  padding: 8px;
+  padding: 10px;
+  margin: 0 5px;
+  &:hover {
+    opacity: 0.5;
+    cursor: pointer;
+  }
 `;
