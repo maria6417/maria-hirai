d613221b7d6f326e64df9fc1fb8d48d6154416df
hmaria <mh6417a@gmail.com>
2022-10-30 14:48:05 -0700

    added reveal animation

--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -1,20 +1,29 @@
 import styled from 'styled-components';
 
 export const theme = {
-  main: '#B5838D',
+  // main: '#B5838D',
+  slate: '#ccd6f6',
+  lightest: '#ffcdb2',
+  light: '#ffb4a2',
+  middle: '#e5989b',
+  middark: '#b5838d',
   dark: '#6D6875',
-  xxl: '40px',
+  darker: '#47444d',
+  xxxl: '40px',
+  xxl: '30px',
   xl: '20px',
   lg: '18px',
   md: '16px',
   sm: '14px',
   xs: '13px',
   xxs: '12px',
+  minHeightDesktop: 'calc(100vh - 115px)',
+  minHeightMobile: 'calc(100vh - 98px',
 };
 
 export const Section = styled.section`
-  width: 60%;
-  height: 100vh;
+  width: 70%;
+  min-height: 100vh;
   margin: auto;
   display: flex;
   flex-direction: column;
@@ -24,11 +33,10 @@ export const Section = styled.section`
 
 export const Button = styled.button`
   background-color: white;
-  color: ${(props) => props.theme.main};
-  border: ${(props) => `1px solid ${props.theme.main}`};
+  color: ${(props) => props.theme.middark};
+  border: ${(props) => `1px solid ${props.theme.middark}`};
   border-radius: 5px;
   padding: 10px;
-  margin: 0 5px;
   &:hover {
     opacity: 0.5;
     cursor: pointer;
--- client/src/utils/reveal.js
+++ client/src/utils/reveal.js
@@ -1,5 +1,6 @@
 export const containerRevealOptions = {
   delay: 250,
   duration: 500,
-  scale: 0.85
+  scale: 0.85,
+  reset: true,
 };
