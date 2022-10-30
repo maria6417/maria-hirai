1dfe7e4e5e396f9d52b3560048182e7f41d90d39
hmaria <mh6417a@gmail.com>
2022-10-30 14:47:50 -0700

    modified Home component with pic

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,51 +1,86 @@
 import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import ScrollReveal from 'scrollreveal';
-import { Section } from './styles';
+import { Section, Button } from './styles';
 import { containerRevealOptions } from '../utils/reveal';
+import maria from '../assets/mariah.jpg';
 
 export default function Home() {
   const revealContainer = useRef(null);
   useEffect(() => {
-    ScrollReveal().reveal(revealContainer.current, containerRevealOptions);
+    if (revealContainer.current) {
+      ScrollReveal().reveal(revealContainer.current, containerRevealOptions);
+    }
   }, []);
 
   return (
-    <Section ref={revealContainer}>
-      <H1>
-        Hi, my name is
-      </H1>
-      <H2>
-        Maria Hirai.
-      </H2>
-      <H3>
-        I&apos;m a Software Engineer.
-      </H3>
-      <P>
-        I&apos;m a Software Engineer focusing on building exceptional user experience.
-      </P>
-    </Section>
+    <Container>
+      <FlexSection ref={revealContainer}>
+        <ProfilePic src={maria} alt="maria hirai" />
+        <div>
+          <H1>
+            Hello!
+          </H1>
+          <H2>
+            I am Maria Hirai.
+            <br />
+            A Full Stack Engineer
+            <br />
+            based in Los Angeles.
+          </H2>
+          <Button type="button" style={{ width: '100px' }}>Say Hi!</Button>
+        </div>
+      </FlexSection>
+    </Container>
   );
 }
 
+const ProfilePic = styled.img`
+  margin-top: 20px;
+  width: 480px;
+  height: 500px;
+  object-fit: cover;
+`;
+
 const H1 = styled.h1`
-  font-size: ${(props) => props.theme.sm};
+  font-size: ${(props) => props.theme.xxl};
+  font-weight: 800;
   color: ${(props) => props.theme.main};
-  margin: 0px 0px 30px 4px;
+  margin: 0px 0px 30px 0px;
 `;
 
 const H2 = styled.h2`
-  font-size: ${(props) => props.theme.xxl};
-  font-weight: 900;
-`;
-
-const H3 = styled.h3`
-  font-size: ${(props) => props.theme.xxl};
+  font-size: ${(props) => props.theme.xxxl};
   color: ${(props) => props.theme.dark};
   font-weight: 900;
+  margin: 0px 0px 30px 0px;
 `;
 
 const P = styled.p`
-  font-size: ${(props) => props.theme.sm};
+  font-size: ${(props) => props.theme.md};
   color: ${(props) => props.theme.dark};
+`;
+
+const FlexSection = styled(Section)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  min-height: calc(100vh - 100px);
+  @media (max-width: 768px) {
+    min-height: cal(100vh - 90px);
+  }
+`;
+
+const Container = styled.div`
+  background-color: #fffbf9;
+  /* &:after {
+    content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.25);
+  } */
 `;
