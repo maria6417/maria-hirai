80338558453e2195b1022503698279a00b73cdca
Maria Hirai <mhirai@krafton.com>
2023-07-21 13:39:21 -0700

    delete all files for refactor

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-
-// eslint-disable-next-line react/jsx-filename-extension, no-undef
-ReactDOM.render(<App />, document.getElementById('root'));
--- client/src/utils/reveal.js
+++
@@ -1,6 +0,0 @@
-export const containerRevealOptions = {
-  delay: 250,
-  duration: 500,
-  scale: 0.85,
-  reset: true,
-};
